dbff10efcf0e73063a6fceafb0efc7f3fcd8bc22
Helena Rosa <helena.rosa@aluno.senai.br>
2026-01-30 17:21:03 -0300

    fazendo o site do croche de tarde

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 import random
 
 app= Flask(__name__)
@@ -8,9 +8,26 @@ app= Flask(__name__)
 def pagina_principal():
     return render_template("principal.html")
 
-@app.route("/sobre")
+@app.route("/sobre", methods=["GET"])
 def sobre_mim ():
     return render_template("sobre.html")
+
+@app.route("/login", methods=["GET"])
+def login():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login_post():
+   usuario = request.form.get ("usuario")
+   senha= request.form.get ("senha")
+
+   if usuario == "Helena" and senha == "1234":
+       return "Voce acessou a pagina restrita"
+   else: 
+       return render_template ("login.html", erro = "Acesso Negado!")
+   
+
+
 
 
 
